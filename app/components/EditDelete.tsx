bc7657b0f2fbedb00abc70e84dff628d3fd35a6e
Weeksly1312 <S.elhallaoui01@gmail.com>
2024-01-07 15:38:39 +0100

    add edit delete buttons component

---
+++ app/components/EditDelete.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+
+const EditDelete = () => {
+  return (
+    <>
+      <button className="bg-yellow-500 text-white border-yellow-600 border rounded-lg px-4 py-1 m-2">
+        Edit
+      </button>
+      <button className="bg-red-500 text-white border-red-600 border rounded-lg px-4 py-1 m-2">
+        Delete
+      </button>
+    </>
+  );
+};
+
+export default EditDelete;

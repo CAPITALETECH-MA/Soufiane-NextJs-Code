cbe60f016bd6a1d78f0eb06239a45f606b4e4c6c
Weeksly1312 <S.elhallaoui01@gmail.com>
2024-01-07 13:24:02 +0100

    add page /users

---
+++ app/users/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Users",
+  description: "User page",
+};
+
+const pages = () => {
+  return <div>pages</div>;
+};
+
+export default pages;

22d769471a30eb0f6c28a3f1746109efa04f9c3d
Weeksly1312 <S.elhallaoui01@gmail.com>
2024-01-08 17:08:51 +0100

    add some styles

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,52 +1,88 @@
+"use client";
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+// import classnames from 'classnames'
 
 const logoSrc = "/CapTechBg.png";
 const plusIcon = "/plus.png";
 
-const links = [
-  { href: "/", label: "Home" },
-  { href: "/pages/users", label: "Users" },
-  { href: "/pages/new", label: "Add Users" },
-];
+type LinkItem = {
+  href: string;
+  label: string;
+};
 
 const Navbar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
+
+  const links: LinkItem[] = [
+    { href: "/", label: "Home" },
+    { href: "/pages/users", label: "Users" },
+    { href: "/pages/new", label: "Add Users" },
+  ];
+
   return (
-    <nav className="bg-black text-white p-4">
-      <div className="flex justify-between items-center">
-        {/* Logo */}
-        <div className="flex items-center">
-          <Image
-            src={logoSrc}
-            alt="Logo"
-            width={220}
-            height={120}
-            className="rounded-full"
-          />
+    <>
+      <nav className="bg-black text-white p-4">
+        <div className="flex justify-between items-center">
+          {/* Logo */}
+          <div className="flex items-center">
+            <Image
+              src={logoSrc}
+              alt="Logo"
+              width={220}
+              height={120}
+              className="rounded-full"
+            />
+          </div>
+
+          {/* Links */}
+          <ul className="flex space-x-4">
+            {links.map((link) => (
+              <li
+                key={link.href}
+                className="flex items-center space-x-2 hover:bg-[#124366] hover:rounded-md p-2"
+              >
+                {/* Tenary condition  */}
+                <Link
+                  className={`${
+                    link.href === currentPath
+                      ? "text-blue-900"
+                      : "text-blue-500"
+                  }`}
+                  href={link.href}
+                >
+                  {link.label}
+                </Link>
+
+                {/*  Classes method */}
+                {/* <Link
+                  className={classnames({
+                    "text-blue-900": link.href === currentPath,
+                    "text-blue-500": link.href !== currentPath,
+                  })}
+                  href={link.href}
+                >
+                  {link.label}
+                </Link> */}
+
+                {/* plus icon */}
+                <Image
+                  src={plusIcon}
+                  alt="Plus Icon"
+                  width={20}
+                  height={20}
+                  className="transition-transform transform hover:scale-150"
+                />
+              </li>
+            ))}
+          </ul>
         </div>
-
-        {/* Links */}
-        <ul className="flex space-x-4">
-          {links.map((link) => (
-            <li
-              key={link.href}
-              className="flex items-center space-x-2 hover:bg-[#124366] hover:rounded-md p-2"
-            >
-              <Link href={link.href}>{link.label}</Link>
-
-              {/* plus icon */}
-              <Image
-                src={plusIcon}
-                alt="Plus Icon"
-                width={20}
-                height={20}
-                className="transition-transform transform hover:scale-150"
-              />
-            </li>
-          ))}
-        </ul>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 

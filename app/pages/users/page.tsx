c2e27ccf39b02ebc13b616f30f1713d00dc2c800
Weeksly1312 <S.elhallaoui01@gmail.com>
2024-01-08 17:19:43 +0100

    try the caching

--- app/pages/users/page.tsx
+++ app/pages/users/page.tsx
@@ -14,17 +14,22 @@ interface User {
 }
 
 const usersData = async () => {
-  const data = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await fetch("https://jsonplaceholder.typicode.com/users",
+  { next: { revalidate: 3600 } }
+  );
   const users: User[] = await data.json();
 
   return (
     <>
-      <h3>Users</h3>
+      <h3>
+        Users
+      </h3>
       <ul>
         {users.map((user) => (
           <li key={user.id}>
             {user.name}
             <EditDelete />
+            <p>{new Date().toLocaleTimeString()}</p>
           </li>
         ))}
       </ul>

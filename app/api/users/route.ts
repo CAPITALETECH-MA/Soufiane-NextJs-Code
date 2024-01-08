e9062aa33ca02464e3c28febb286c23a0db02a8f
Weeksly1312 <S.elhallaoui01@gmail.com>
2024-01-08 18:55:39 +0100

    my first api POST

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,5 +1,24 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "../../../prisma/client";
 
-export async function POST(req: NextRequest){
-    const body = await req.json()
+const usersValidator = z.object({
+  name: z.string().min(8),
+  email: z.string().email(),
+  age: z.number().min(18).max(80),
+});
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const validation = usersValidator.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+
+  const newUser = await prisma.user.create({
+    data: { name: body.name, email: body.email, age: body.age },
+  });
+
+  return NextResponse.json(newUser, { status: 201 });
 }

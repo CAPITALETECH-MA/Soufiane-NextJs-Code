738126ef2d756ac4390ede16aa0b176ab54a72db
Weeksly1312 <S.elhallaoui01@gmail.com>
2024-01-09 18:42:55 +0100

    add the UserForm component

--- app/pages/new/page.tsx
+++ app/pages/new/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { Metadata } from "next";
-import UserForm from "@/app/components/userForm";
+import UserForm from "@/app/components/UserForm";
 
 export const metadata: Metadata = {
   title: "Add Users",
